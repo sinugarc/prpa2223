04d28eb903722d5dcae9e1b59a78b0ca68b8a565
sinugarc <115789430+sinugarc@users.noreply.github.com>
2023-02-02 10:46:05 +0100

    almost fixed 3

--- 3_procesos.py
+++ 3_procesos.py
@@ -5,14 +5,13 @@ import time
 def f(c):
     for i in range(100):
         temp = c.value + 1
-        print (f"hola soy {current_process().pid}, vuelta: {i}, contador: 
-{c.value}")
+        print (f"hola soy {current_process().pid}, vuelta: {i}, contador: {c.value}")
         time.sleep(0.1)
         c.value = temp
 if __name__ == "__main__":
     N = 8
     lp = []
-    c = Value(’i’, 0)
+    c = Value('i', 0)
     for i in range(N):
         lp.append(Process(target=f, args=(c,)))
     print ("Valor inicial del contador", c.value)
