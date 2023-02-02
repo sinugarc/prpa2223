487667e35803daa1deb9fd7562133793f4c3e7a4
Danima20 <danima20@ucm.es>
2023-02-02 10:45:16 +0100

    Modificacion del 4

--- 4_procesos.py
+++ 4_procesos.py
@@ -5,17 +5,16 @@ from multiprocessing import Value
 def f(c):
     for i in range(100):
         c.value = c.value + 1
-        print (f"hola soy {current_process().pid}, vuelta: {i}, contador: 
-{c.value}")
+        print ("hola soy ",{current_process().pid}, "vuelta: ", {i}," contador:   ", {c.value})
 def g():
     print ("adios")
 if __name__ == "__main__":
     N = 8
     lp = []
-    c = Value(’i’, 0)
+    c = Value('i', 0)
     for i in range(N):
         lp.append(Process(target=f, args=(c,)))
-    print (f"Valor inicial del contador {c.value}")
+    print (f"Valor inicial del contador ", {c.value})
     for p in lp:
         p.start()
     for p in lp:
@@ -23,5 +22,5 @@ if __name__ == "__main__":
     q = Process(target=g)
     q.start()
     q.join()
-    print (f"Valor final del contador {c.value}")
+    print (f"Valor final del contador: ", {c.value})
     print ("fin")
