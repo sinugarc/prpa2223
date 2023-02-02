f189981115ee5c0d5cf824d02d7fc4245f6d40e6
Danima20 <danima20@ucm.es>
2023-02-02 10:33:17 +0100

    Modificacion

--- 1_procesos.py
+++ 1_procesos.py
@@ -5,15 +5,15 @@ from multiprocesing import Process
 
 def f(value):
     for i in range(3):
-        print (f"hola soy {value} vuelta {i})
-        time.sleep(random.random()/3)
+        print ("hola soy {value} vuelta {i})
+        time.sleep(random.random()/3")
 def g():
     print ("adios")
 
 if __name__ == "__main__":
     N=10
     lp = []
-    for i in range(N)
+    for i in range(N):
         lp.append(Process(target=f,args=(f"ana {i}",)))
     for p in lp:
         p.start()
